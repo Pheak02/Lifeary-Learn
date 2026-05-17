aa8b3c2ce59290ac454614fdd9886497150eafb2
Pheak02 <saingsopheak02@gmail.com>
2026-05-17 13:48:44 +0700

    learn: create mvc portlet module

---
+++ pheak-learn-liferay/modules/person-blog/src/main/java/lifeary/learn/mvc/portlet/constants/PersonBlogPortletKeys.java
@@ -0,0 +1,11 @@
+package lifeary.learn.mvc.portlet.constants;
+
+/**
+ * @author saing
+ */
+public class PersonBlogPortletKeys {
+
+	public static final String PERSONBLOG =
+		"lifeary_learn_mvc_portlet_PersonBlogPortlet";
+
+}
---
+++ pheak-learn-liferay/modules/person-blog/src/main/resources/META-INF/resources/view.jsp
@@ -0,0 +1,5 @@
+<%@ include file="/init.jsp" %>
+
+<p>
+	<b><liferay-ui:message key="personblog.caption"/></b>
+</p>
